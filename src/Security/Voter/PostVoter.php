f0956b03b75548f1c474276a447a2146699b3421
TristanBonnal <bonnal.tristan@hotmail.fr>
2022-04-08 23:31:58 +0200

    voters for moderators and andmin

--- src/Security/Voter/PostVoter.php
+++ src/Security/Voter/PostVoter.php
@@ -4,15 +4,19 @@ namespace App\Security\Voter;
 
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class PostVoter extends Voter
 {
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+    
     protected function supports(string $attribute, $subject): bool
     {
-        // replace with your own logic
-        // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, ['POST_EDIT', 'POST_VIEW'])
+        return in_array($attribute, ['POST_EDIT', 'POST_DELETE'])
             && $subject instanceof \App\Entity\Post;
     }
 
@@ -24,17 +28,8 @@ class PostVoter extends Voter
             return false;
         }
 
-        // ... (check conditions and return true to grant permission) ...
-        switch ($attribute) {
-            case 'POST_EDIT':
-                // logic to determine if the user can EDIT
-                // return true or false
-                break;
-            case 'POST_VIEW':
-                // logic to determine if the user can VIEW
-                // return true or false
-                break;
-        }
+        // Autorise si l'utilisateur loggé est l'auteur du post, ou un admin
+        return $user == $subject->getUser() || $this->security->isGranted('ROLE_ADMIN');
 
         return false;
     }
